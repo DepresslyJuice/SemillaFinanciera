9714f4aa0d3fee30a70857371d0be49771bc712e
Juan Varela <varelajuandiego8@gmail.com>
2025-04-26 18:31:43 -0500

    Implementación del WelcomeSection.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
+import WelcomeSection from "./pages/WelcomeSection.tsx";
 
 function App() {
     return (
         <>
             <Header />
             <main>
+                <WelcomeSection />
                 <Home />
             </main>
             <Footer />
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,8 @@ function Home() {
         <section id="inicio" style={{ padding: '2rem' }}>
             <h2 style={{ fontSize: '2rem', marginBottom: '1rem' }}>Bienvenido a tu camino hacia la libertad financiera</h2>
             <p style={{ fontSize: '1.2rem', maxWidth: '600px' }}>
-                Aprende a manejar tu dinero, ahorrar de manera inteligente, diseñar presupuestos efectivos y hacer un mejor uso de tus tarjetas de crédito.
+                Aprende a manejar tu dinero, ahorrar de manera inteligente, diseñar presupuestos efectivos y hacer un
+                mejor uso de tus tarjetas de crédito.
             </p>
             <img
                 src="https://cdn-icons-png.flaticon.com/512/2331/2331970.png"
